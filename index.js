f83ea5719f269b4ca061618acf0a80975bac4980
elad1978 <efrathalev@gmail.com>
2023-06-25 10:04:15 +0300

    2

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ const PORT = 8070;
 
 let products = [
   { id: 1, name: "ASUS-nf1", price: 5000, category: "lep-tops" },
-  { id: 1, name: "Galaxy", price: 3000, category: "phones" },
+  { id: 2, name: "Galaxy", price: 3000, category: "phones" },
+  { id: 2, name: "Galaxy2", price: 3500, category: "phones" },
 ];
 
 app.get("/api/products", (req, res) => {
   res.json(products);
+});
+
+app.get("/api/products/:category", (req, res) => {
+  const id = req.params.category;
+  if (!id) {
+    res.status(400).send("there in no category");
+    return;
+  }
+  const catProducts = products.filter((p) => p.category == category);
+  if (!product) {
+    res.status(404).send("no data was found");
+    return;
+  }
+  res.json(product);
 });
 
 app.listen(PORT, () => {
